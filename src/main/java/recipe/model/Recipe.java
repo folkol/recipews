78bb550902881166600ec0b8a0b4f32944165d7a
Folkol <mattias4@kth.se>
2014-06-02 01:32:51 +0200

    Can accept and render Recipe JSON

---
+++ src/main/java/recipe/model/Recipe.java
@@ -0,0 +1,18 @@
+package recipe.model;
+
+import static java.lang.String.format;
+
+
+public class Recipe {
+
+    public String title;
+    public String ingredients;
+    public String[] image;
+
+    @Override
+    public String toString() {
+        return format("%s\n%s\n%s", title, ingredients, image.length);
+    }
+
+
+}
--- src/main/java/recipe/rest/RecipeWS.java
+++ src/main/java/recipe/rest/RecipeWS.java
@@ -1,10 +1,15 @@
 package recipe.rest;
 
 import javax.inject.Inject;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 import recipe.ejb.RecipeService;
+import recipe.model.Recipe;
 
 
 @Path("/recipe")
@@ -15,5 +20,12 @@ public class RecipeWS {
     @GET
     public String list() {
         return recipeService.list();
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Recipe add(Recipe recipe) {
+        return recipe;
     }
 }
