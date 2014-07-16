733e493b99155485cca3fe1e6520df34077ab986
Folkol <mattias4@kth.se>
2014-07-16 19:04:18 +0200

    Now using all resources

--- src/main/java/ws/WebService.java
+++ src/main/java/ws/WebService.java
@@ -1,19 +1,9 @@
 package ws;
 
-import static java.util.Arrays.asList;
-
-import java.util.HashSet;
-import java.util.Set;
-
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
 
 @ApplicationPath("/ws")
 public class WebService extends Application {
-
-    @Override
-    public Set<Class<?>> getClasses() {
-        return new HashSet<>(asList(RecipesResource.class));
-    }
 }
